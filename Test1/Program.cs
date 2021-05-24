c019d7baba0264f51facb6b107b9debff1ef5009
maggie <maggie.tamanini@gmail.com>
2021-05-24 11:31:02 -0400

    Response added

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -6,8 +6,38 @@ namespace Test1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I'm a little teapot.");
+            Console.WriteLine("Sing along with me!");
+
+            string repeat;
+
+            do
+            {
+
+                Console.Write("I'm a little teapot.");
+                Console.WriteLine("");
+                string responseONE = Console.ReadLine();
+
+                if (responseONE.ToLower().StartsWith("short and stout"))
+                {
+                    Console.WriteLine("Here is my handle, here is my spout!");
+                }
+
+
+                else
+                {
+                    Console.WriteLine("Those aren't the words!");
+                }
+
+                    Console.WriteLine("Try again?");
+                    Console.WriteLine("");
+                    repeat = Console.ReadLine();
+           
+
+            } while (string.Equals(repeat, "yes", StringComparison.OrdinalIgnoreCase));
+
+            }
+
+      
 
         }
     }
-}
